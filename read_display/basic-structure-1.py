9a0b8f87a163aacc7fbb7eb85918bb3db5ddb12a
vinod patil <vinodpatil@vinods-MacBook-Air.local>
2023-05-26 17:02:45 +0530

    commit2

---
+++ read_display/basic-structure-1.py
@@ -0,0 +1,16 @@
+import yaml
+
+my_file = "basic-structure-1.yaml"
+
+with open(my_file, 'r', encoding='utf8') as stream:
+
+    try:
+        data = yaml.safe_load(stream)
+        for key, value in data.items():
+            print(f"KEY : {key}")
+            print(f"VALUE : {value}")
+            dt = type(value)
+            print(f"Data type od {value} is {dt}")
+            
+    except yaml.YAMLError as err:
+        print(err)
